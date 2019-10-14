60e492959d845bd61bf4f64c562196cc5f95c3fc
Billyyyyy3320 <newsbielt703@gmail.com>
2019-10-14 23:03:24 +0800

    feat: handle missing shortname option

--- src/index.js
+++ src/index.js
@@ -1,11 +1,22 @@
-const { path } = require("@vuepress/shared-utils");
+const { path, logger, chalk } = require("@vuepress/shared-utils");
 
-module.exports = options => ({
-  name: "disqus-comment",
-
-  enhanceAppFiles: [path.resolve(__dirname, "enhanceAppFile.js")],
-
-  define: {
-    DISQUS_OPTIONS: JSON.stringify(options)
+module.exports = options => {
+  if (!options.shortname) {
+    logger.warn(
+      `[vuepress-plugin-disqus-comment] Failed: ${chalk.cyan(
+        "shortname is required"
+      )}`
+    );
+    return;
   }
-});
+
+  return {
+    name: "disqus-comment",
+
+    enhanceAppFiles: [path.resolve(__dirname, "enhanceAppFile.js")],
+
+    define: {
+      DISQUS_OPTIONS: JSON.stringify(options)
+    }
+  };
+};
