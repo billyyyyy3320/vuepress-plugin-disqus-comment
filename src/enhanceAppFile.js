b39ffe798fda3785249ed4df2b8b9375f9bb834c
Billyyyyy3320 <newsbielt703@gmail.com>
2019-10-14 15:53:35 +0800

    feat: handle possible lang exception

--- src/enhanceAppFile.js
+++ src/enhanceAppFile.js
@@ -9,11 +9,20 @@ export default ({ Vue }) => {
   Vue.component("vue-disqus", {
     functional: true,
     render(h, { parent, props }) {
+      // Get default lang
+      let DefaultLanguage;
+      // The default value is en-US, but it's not a option for Disqus localization
+      if (parent.$lang === "en-US") {
+        DefaultLanguage = "en";
+      } else {
+        DefaultLanguage = parent.$lang.replace(/\-/, "_");
+      }
+
       // SSR-friendly
       if (parent._isMounted) {
         return h(DisqusComponent, {
           // Priority: VuePress's $lang as default language < global configuration < props
-          props: Object.assign({ language: parent.$lang }, options, props)
+          props: Object.assign({ language: DefaultLanguage }, options, props)
         });
       } else {
         parent.$once("hook:mounted", () => {
